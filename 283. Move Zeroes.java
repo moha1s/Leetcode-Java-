cba317e149dacd750c02ecef24ee5574ff5a1f37
tz136 <1992leozhang@gmail.com>
2017-03-15 22:52:31 -0400

    up

---
+++ 283. Move Zeroes.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int cur=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[cur]=nums[i];
+                cur++;
+            }
+        }
+        for(;cur<nums.length;cur++){
+            nums[cur]=0;
+        }
+        
+    }
+}
